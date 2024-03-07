1a6070220e5b49ed0f979435cc2c6db46bbf2617
Firooz Housainee <fhousainee@launchcg.com>
2024-03-07 12:47:06 -0800

    Creating new Project

---
+++ src/main/java/Selenium_Java_Framework/Selenium_Java_Framework/App.java
@@ -0,0 +1,13 @@
+package Selenium_Java_Framework.Selenium_Java_Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
